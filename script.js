5689aef322a3c21aacf74633d4e2ff511195a898
Elias Gabriel <123685353+EliasGabrielCF@users.noreply.github.com>
2026-04-07 08:52:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function scrollProjetos(){
+
+document.getElementById("projetos")
+.scrollIntoView({behavior:"smooth"})
+
+}
+
+function alertProjeto(){
+
+alert("Esse projeto está hospedado no meu GitHub!")
+
+}
